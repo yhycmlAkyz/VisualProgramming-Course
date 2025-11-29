b81ead1613b0499ba18e22309e5ac9bb1ea00d5d
Yahya Cemal AKYÜZ <kemalcemala@gmail.com>
2025-11-29 12:35:12 +0300

    Elmas Şekli Oluşturucu (Diamond Display)

---
+++ ElmasCizimi/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ElmasCizimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int satir = 1; satir <= 5; satir++)
+            {
+
+                for (int bosluk = 1; bosluk <= (5 - satir); bosluk++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int yildiz = 1; yildiz <= (2 * satir) - 1; yildiz++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            for (int satir = 4; satir >= 1; satir--)
+            {
+                for (int bosluk = 1; bosluk <= (5 - satir); bosluk++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int yildiz = 1; yildiz <= (2 * satir) - 1; yildiz++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
