81a6b333dbf111bb9ba0fc3678c8439b3ce21a3c
Yahya Cemal AKYÜZ <kemalcemala@gmail.com>
2025-12-26 14:09:54 +0300

    CommissionEmployee

---
+++ CommissionEmployee/CommissionEmployee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CommissionApp
+{
+    class CommissionEmployeeTest
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("--- Test Başlıyor ---\n");
+
+            var employee = new CommissionEmployee("Sue", "Jones",
+                "222-22-2222", 10000.00M, .06M);
+
+            Console.WriteLine("Employee information obtained by properties and methods:\n");
+            Console.WriteLine($"First name is {employee.FirstName}");
+            Console.WriteLine($"Last name is {employee.LastName}");
+            Console.WriteLine($"Gross sales are {employee.GrossSales:C}");
+            Console.WriteLine($"Earnings are {employee.Earnings():C}");
+
+            Console.WriteLine("\nUpdating information...");
+            employee.GrossSales = 5000.00M;
+            employee.CommissionRate = .1M;
+
+            Console.WriteLine("\nUpdated employee information obtained by ToString:\n");
+            Console.WriteLine(employee);
+            Console.WriteLine($"Earnings: {employee.Earnings():C}");
+
+            Console.WriteLine("\nÇıkmak için bir tuşa basın...");
+            Console.ReadKey();
+        }
+    }
+}
